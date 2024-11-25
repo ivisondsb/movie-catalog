91a44ec6a1f71a964654b0aa133ca548075b47ea
ivisondsb <ivisonk0@gmail.com>
2024-11-25 17:10:49 -0300

    ep 1

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import { Container, SearchContainer, Input, SearchButton } from "./styles";
+import Header from "../../components/Header";
+import { Feather } from "@expo/vector-icons";
+
+export default function Home() {
+  return (
+    <Container>
+      <Header title="React Prime" />
+
+      <SearchContainer>
+        <Input placeholder="Ex: Vingadores" placeholderTextColor="#ddd" />
+
+        <SearchButton>
+          <Feather name="search" size={30} color="#FFF" />
+        </SearchButton>
+      </SearchContainer>
+    </Container>
+  );
+}
---
+++ src/pages/Movies/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Movies() {
+  return (
+    <View>
+      <Text>Tela meus filmes</Text>
+    </View>
+  );
+}
